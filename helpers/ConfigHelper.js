7ada0a367285385aa0b5217fd6bba6b09f9ff064
Jared Huffstutler <jaredhuff85@gmail.com>
2020-10-23 12:09:07 -0700

    showing embed for config settings
    Signed-off-by: Jared Huffstutler <jaredhuff85@gmail.com>

--- helpers/ConfigHelper.js
+++ helpers/ConfigHelper.js
@@ -1,8 +1,16 @@
 const { writeFile } = require('fs');
 const { join } = require('path');
+const { MessageEmbed } = require('discord.js');
+
+// Constants.
+const { CURRENCY_SYMBOL_FIELD } = require('../constants/commands');
 
 // Config file.
-const { economyConfig } = require('../config');
+
+const {
+  economyConfig,
+  defaultEconomyConfig: { currencySymbol },
+} = require('../config');
 
 module.exports = class ConfigHelper {
   static saveConfig(message, configField, value, successMessage) {
@@ -17,8 +25,22 @@ module.exports = class ConfigHelper {
         return message.reply(error.message);
       }
 
-      return message.channel
-        .send(`${successMessage} ${value}`);
+      // console.log(message.author)
+
+      // `<${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`
+
+      const { author } = message;
+      const { username } = author;
+
+      const symbol = economyConfig.currencySymbol || currencySymbol;
+      const valuePrepend = configField !== CURRENCY_SYMBOL_FIELD ? symbol : '';
+
+      const embed = new MessageEmbed()
+        .setColor('GREEN')
+        .setAuthor(username, author.displayAvatarURL())
+        .setDescription(`${successMessage} ${valuePrepend}${value}`);
+
+      message.channel.send(embed);
     });
   }
 };
