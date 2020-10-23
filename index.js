c6fada057af0c82c84a042842fd16479f1b3317f
Jared Huffstutler <jaredhuff85@gmail.com>
2020-10-23 06:20:43 -0700

    added eslint
    Signed-off-by: Jared Huffstutler <jaredhuff85@gmail.com>

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-const token = process.env.TOKEN;
 
 function fuuckYeah(message) {
-	const output = 'Fuuck Yeah Our Custom Bot Is Alive';
-	message.channel.send(output);
+  const output = 'Fuuck Yeah Our Custom Bot Is Alive';
+  message.channel.send(output);
 }
 
 const commands = new Map();
 commands.set('fuuckYeah', fuuckYeah);
 
 client.on('message', message => {
-	if (message.content[0] === '!') {
-		const command = message.content.split(' ')[0].substr(1);
-		if (commands.has(command)) {
-			commands.get(command)(message);
-		}
-	}
+  if (message.content[0] === '!') {
+    const command = message.content.split(' ')[0].substr(1);
+    if (commands.has(command)) {
+      commands.get(command)(message);
+    }
+  }
 });
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
-client.login(token);
+client.login(process.env.TOKEN);
