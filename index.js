f91199b109c0e338c8a2b9cda7af776d9189e0c3
Jared Huffstutler <jaredhuff85@gmail.com>
2020-10-23 05:57:33 -0700

    initial setup
    Signed-off-by: Jared Huffstutler <jaredhuff85@gmail.com>

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+const token = process.env.TOKEN;
+
+function fuuckYeah(message) {
+	const output = 'Fuuck Yeah Our Custom Bot Is Alive';
+	message.channel.send(output);
+}
+
+const commands = new Map();
+commands.set('fuuckYeah', fuuckYeah);
+
+client.on('message', message => {
+	if (message.content[0] === '!') {
+		const command = message.content.split(' ')[0].substr(1);
+		if (commands.has(command)) {
+			commands.get(command)(message);
+		}
+	}
+});
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(token);
